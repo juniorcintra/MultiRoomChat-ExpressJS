1bd3f49fa440c908f3e0a0a6c64be9f3b497f458
Junior Cintra <junior.developer@outlook.com.br>
2021-08-10 00:07:04 -0300

    implementando socket.io

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = require("./config/server");
 
-app.listen(4000, function () {
+var server = app.listen(80, function () {
   console.log("Servidor Online");
 });
+
+require("socket.io").listen(server);
