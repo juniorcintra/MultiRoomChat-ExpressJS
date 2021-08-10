cc63950d67e880bde10e5e3a74e2e75218629c8d
Junior Cintra <junior.developer@outlook.com.br>
2021-08-10 00:14:56 -0300

    implementando conecxao websocket

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ var server = app.listen(80, function () {
   console.log("Servidor Online");
 });
 
-require("socket.io").listen(server);
+var io = require("socket.io").listen(server);
+
+io.on("connection", function (socket) {
+  console.log("Usuario conectou");
+
+  socket.on("disconnect", function () {
+    console.log("Usuario desconectou");
+  });
+});
